75599aa2bc466594705789fee8ccd881c1595710
Katy Deng <deng2017.xq@gmail.com>
2018-04-05 09:26:55 -0700

    horizontal right function

--- main.js
+++ main.js
@@ -4,11 +4,16 @@ var boardTokenArray = [];
 var divNum; // the number that represents the disc_container  
 var boxNum = 0;
 var clickTime = 0;
+var containerCol;
+var containerRow;
+var currentBoxLocation; 
 
 
 
 function intiApp(){
-	create_board()
+	create_board();
+	definePlayerTurn();
+	// checkBoxReliability();
 	$('.disc_container').on('click', horizontalClickRight);// box_click is when game stars
 	$('#28').off('click');
 	$('#37').off('click');
@@ -20,7 +25,6 @@ function create_board(){
 	for(var nestedArr = 0; nestedArr < 8; nestedArr++ ){
 		var nestedArrSet  = [];
 		boardTokenArray[nestedArr] = [];
-		console.log(boardTokenArray);
         gameBoardArray.push(nestedArrSet);
 		for(var arrItem = 0; arrItem < 8; arrItem++){
 			nestedArrSet.push(0);
@@ -57,16 +61,16 @@ function makeToken(color){
 	return $('<div>', {class: 'transparent_disc'}).addClass(color);
 }	
 
-function box_click(element){ // box_click is when game stars.... element is the box that clicked 
 
+
+function box_click(element){ // box_click is when game stars.... element is the box that clicked 
 		if(clickTime % 2 === 0 && clickTime < 60){
 			boxRow = $(element).attr('row'); //element is the equail to this. which refer to the actually disc container that is clikced
 			boxCol = $(element).attr('col');
-			makeToken('black').appendTo(element);
+			makeToken('black').appendTo(element);  // place the disc in black to the disc container 
 			clickTime++;
-			gameBoardArray[boxRow][boxCol] = 2
+			gameBoardArray[boxRow][boxCol] = 2;
 			$(element).off('click'); //disables click after black disc is formed 
-
 
 		}else{
 			if(clickTime < 60){
@@ -74,61 +78,104 @@ function box_click(element){ // box_click is when game stars.... element is the 
 			makeToken('green').appendTo(element);
 			}
 			clickTime++;
-			gameBoardArray[boxRow][boxCol] = 1
+			gameBoardArray[boxRow][boxCol] = 1;	
 			$(element).off('click');        //disables click after white disc is formed
 		}
 	
 	// var disc = $(this).addClass('black');
-	console.log(divNum);
+	// console.log(divNum);
 	console.log(gameBoardArray)
 }
 
 
 
+/// function to check for the color in player's turn; 
+function definePlayerTurn(){
+	if(clickTime % 2 === 0 && clickTime < 60){ //clickTime is equal to even 
+        // player 1 turn 
+        checkColorInPlayerTurn('black');
+
+ 
+   }else {
+       // player 2 turn
+        checkColorInPlayerTurn('green');
+   }
+}
 
 
-function horizontalClickRight(){
-	
-	for( var rowIndex=0 ; rowIndex < 8; rowIndex++){ // index = 0 
-        for(var colIndex = 0; colIndex < 8; colIndex++){
-        	gameBoardArray[rowIndex][colIndex];
-        	if(gameBoardArray[rowIndex][colIndex] === 2 ){ // local the black 
-				colIndex++; // nestedArrayIndex = 1;
- 				if(gameBoardArray[rowIndex][colIndex]  === 1 ){ //nestedArrayIndex = 1; 
-                     colIndex++;  // discBoxIndex = 2; 
-                     if(gameBoardArray[rowIndex][colIndex] === 1){
-                        colIndex++;  // nestedArrayIndex = 3 
-
-                     }else{ // mainArrayIndex[nestedArrayIndex] = 0; 
-                       box_click(this);
-                       gameBoardArray[rowIndex][colIndex] = 2; 
-                       gameBoardArray[rowIndex][colIndex-1] = 2;
-                       boardTokenArray[rowIndex][colIndex-1].find('.transparent_disc').removeClass('green').addClass('black');
-
-                     }
-
-
-                }else{
-                    return; // look to the left
- 				}
-        	}
-        }
-
-		// if(nestedArrInGBA[arrayIndex]=2){ //nestedArrInGBA[0];
-		// 	if(gameBoardArray[arrayIndex+1] !== 2 && gameBoardArray[arrayIndex+1] === 1){ // to the right 
-		// 		arrayIndex+2// we want to move over 2 from the current position of that player 
-		// 	} else if (gameBoardArray[arrayIndex+1] === 0 && ){ 
-		// 		//break
-		// 	}
-		// }
-      
+function checkColorInPlayerTurn(color){
+	for(var r = 0; r < 8; r++){
+		for(var c = 0; c < 8; c++){
+			if(boardTokenArray[r][c].find('.transparent_disc').hasClass(color)){
+				containerRow = boardTokenArray[r][c].attr('row');  /// r =3; c =3; 
+				containerCol = boardTokenArray[r][c].attr('col');
+				checkBoxReliability();
+				return;
+				// look for boxs that are able to click. make a function that are 
+			}
+		}
 	}
 }
 
+function checkBoxReliability(){
+     currentBoxValue = gameBoardArray[containerRow][containerCol]; /// currentBoxValue =2;
+     ////////Horizontal check 
+     for(var i = containerCol ; i > 0; i--){ // i = 3, i > 0; i-- 3,2,1,0
+     var leftOfTheCurrentBox = gameBoardArray[containerRow][i-1]; //checking to the left   //2, 1, 0
+     var rightOfTheCurrentBox = gameBoardArray[containerRow][i+1]; // checking to the right 
+          if(leftOfTheCurrentBox !== currentBoxValue && leftOfTheCurrentBox !== 0){
+          	
 
-function switchColor(){
+              // leftOfTheCurrentBox = gameBoardArray[containerRow][i-1];
+              // console.log(leftOfTheCurrentBox);
+         
+          }else{
+          	return;
+          }
+     }
+
+
+
+
 
 }
+
+//////////////////////////////////////////////////////////////////////////
+function horizontalClickRight(){
+      
+
+	// for( var rowIndex=0 ; rowIndex < 8; rowIndex++){ // index = 0 
+ //        for(var colIndex = 0; colIndex < 8; colIndex++){
+ //        	gameBoardArray[rowIndex][colIndex];
+ //        	if(gameBoardArray[rowIndex][colIndex] === 2 ){ // local the black 
+	// 			colIndex++; // nestedArrayIndex = 1;
+ // 				if(gameBoardArray[rowIndex][colIndex]  === 1 ){ //nestedArrayIndex = 1; 
+ //                     colIndex++;  // discBoxIndex = 2; 
+ //                     if(gameBoardArray[rowIndex][colIndex] === 1){
+ //                        colIndex++;  // nestedArrayIndex = 3 
+
+ //                     }else{ // mainArrayIndex[nestedArrayIndex] = 0; 
+ //                       box_click(this);
+ //                       gameBoardArray[rowIndex][colIndex] = 2; 
+ //                       gameBoardArray[rowIndex][colIndex-1] = 2;
+ //                       boardTokenArray[rowIndex][colIndex-1].find('.transparent_disc').removeClass('green').addClass('black');
+
+ //                     }
+
+
+ //                }else{
+ //                    return; // look to the left
+ // 				}
+ //        	}
+ //        }
+      
+	// }
+}
+
+//////////////////////////////////////////////////////////////////
+// function switchColor(){
+
+// }
 // function verticalClick(){
 
 // }
